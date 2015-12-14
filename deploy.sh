5fa7df843cb60e8f7385ce87490f488245f6c7f4
Keli <thorkell@me.com>
2015-12-14 14:26:53 +0000

    acceptance success,
    notaði docker tag á nýjustu útgáfuna til að manually feeda hana í
    pullinu á test-vélinni

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ echo Pushing to Docker
 docker push k371/tictactoe
 
 echo Stopping, removing, pulling and running TicTacToe
-ssh vagrant@192.168.33.10 'docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker pull k371/tictactoe
-docker run -p 9000:8080 -d -e "NODE_ENV=production" k371/tictactoe'
+ssh vagrant@192.168.33.10 'docker stop $(docker ps -a -q)'
+ssh vagrant@192.168.33.10 'docker rm $(docker ps -a -q)'
+ssh vagrant@192.168.33.10 'docker pull k371/tictactoe'
+ssh vagrant@192.168.33.10 'docker run -p 9000:8080 -d -e "NODE_ENV=production" k371/tictactoe'
